3378b70c3a3d191a89d3baa0e2c0e29acfbdcc62
1bertojunior <hjunior854@gmail.com>
2023-11-12 20:34:37 -0300

    feat: Add route of app /admin with middleware LevelAccess

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('app.home');
+
+Route::middleware('level_access')->prefix('/app')->group(function() {
+    Route::get('/admin', [AppController::class, 'admin'])->name('app.admin');
+});
