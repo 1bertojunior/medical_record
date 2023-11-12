5e082ed393094c80c4d05f5d12be5d17ccab9d59
1bertojunior <hjunior854@gmail.com>
2023-11-12 20:37:40 -0300

    fix: bug fix of path in the kernel of the LevelAccess

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'level_access' => \App\Http\Middleware\LevelAccessMiddleware\LevelAccessMiddleware::class,
+        'level_access' => \App\Http\Middleware\LevelAccessMiddleware::class
     ];
 }
