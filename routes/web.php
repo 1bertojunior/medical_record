e6c7a305a0b7097bef8a0e718a8ac9cc75c2ff75
1bertojunior <hjunior854@gmail.com>
2023-11-12 13:43:00 -0300

    feat: Add named route for HomeController index as 'app.home'

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('app.home');
