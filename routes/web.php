ea4523934645d5cd7109a113260edf1067c51147
1bertojunior <hjunior854@gmail.com>
2023-11-12 20:56:26 -0300

    chore: Rename authentication middleware to correct_name. Fixed the incorrect middleware name in all files

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('app.home');
 
-Route::middleware('level_access')->prefix('/app')->group(function() {
+Route::middleware('authentication')->prefix('/app')->group(function() {
     Route::get('/admin', [AppController::class, 'admin'])->name('app.admin');
 });
