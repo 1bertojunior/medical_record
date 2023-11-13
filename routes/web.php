4fa6cb1801cf9bc34d1dcafb38d7fbddbe802932
1bertojunior <hjunior854@gmail.com>
2023-11-13 09:35:43 -0300

    feat: Add POST route for login processing

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('app.home');
 
-Route::get('/login', [AuthController::class, 'login'])->name('app.login');
+Route::get('/login', [AuthController::class, 'login'])->name('app.showLogin');
+Route::post('/login', [AuthController::class, 'login'])->name('app.login');
 Route::get('/logout', [AuthController::class, 'logout'])->name('app.logout');
 
 Route::middleware('authentication')->prefix('/app')->group(function() {
