b313c5c0c819bf95795be407afe341578f61886f
1bertojunior <hjunior854@gmail.com>
2023-11-13 16:40:52 -0300

    feat: Prepare admin view for further development

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AppController extends Controller
 {
     public function admin(){
-        return view('app.admin', ['title' => 'Login']);
+        return view('app.admin', ['title' => 'Admin']);
     }
 }
--- resources/views/app/admin.blade.php
+++ resources/views/app/admin.blade.php
@@ -1,3 +1,8 @@
 @extends('app.layouts.app')
 
 @section('title', $title)
+
+@section('content')
+
+
+@endsection
