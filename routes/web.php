11a03f2cd61ffc4e0c6b0d9e502aceac01725afc
1bertojunior <hjunior854@gmail.com>
2023-11-12 21:18:31 -0300

    feat: Add login and logout routes in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('app.home');
+
+Route::get('/login', [AuthController::class, 'login'])->name('app.login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('app.logout');
 
 Route::middleware('authentication')->prefix('/app')->group(function() {
     Route::get('/admin', [AppController::class, 'admin'])->name('app.admin');
