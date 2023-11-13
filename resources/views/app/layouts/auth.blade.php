b7710ad5ec62d15b04110ed4e30b22a408fe29d6
1bertojunior <hjunior854@gmail.com>
2023-11-13 09:03:38 -0300

    style: Update CSS URL in auth view

--- resources/views/app/layouts/auth.blade.php
+++ resources/views/app/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Medical Record')</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
 </head>
 <body>
     <div class="container">
