4f20e25e647455106416ab195a8a7ad5b9364cfb
1bertojunior <hjunior854@gmail.com>
2023-11-13 09:32:17 -0300

    fix/style: Correct form action URL and add @csrf

--- resources/views/app/auth/login.blade.php
+++ resources/views/app/auth/login.blade.php
@@ -5,7 +5,8 @@
 @section('content')
     <div class="login-container">
         <h1>Login do Funcionário (ADM/User)</h1>
-        <form class="login-form" action="processa_login.php" method="post">
+        <form class="login-form" action="{{ route('app.login') }}" method="post">
+            @csrf
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required>
             <label for="senha">Senha:</label>
