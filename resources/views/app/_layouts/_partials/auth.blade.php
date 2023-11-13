abbe93b7ac3394a749491c6dd4ce122e3016f69a
1bertojunior <hjunior854@gmail.com>
2023-11-12 21:45:40 -0300

    fix: path style of the layout auth

--- resources/views/app/_layouts/_partials/auth.blade.php
+++ resources/views/app/_layouts/_partials/auth.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Medical Record')</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <div class="container">
         @yield('content')
     </div>
-    
+
 </body>
 </html>
