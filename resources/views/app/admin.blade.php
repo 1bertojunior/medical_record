4c9ab7e3ec4ce4e6c561f2181a6d2a1133b273b7
1bertojunior <hjunior854@gmail.com>
2023-11-14 11:23:06 -0300

    feat: Add main content to admin page

--- resources/views/app/admin.blade.php
+++ resources/views/app/admin.blade.php
@@ -8,8 +8,51 @@
 
 @include('app.layouts._partials.sidebar')
 
-@include('app.layouts._partials.main_content')
+<main id="main-conteudo-total">
 
+    @include('app.layouts._partials.header')
 
+    <div class="main-conteudo">
+        <div class="conteudo-titulo-main">
+            <h2> Dashboard</h2>
+        </div>
+
+        <div class="dashboard-conteudo">
+            <div class="dashboard-flex">
+                <div class="dashboard-box">
+                    <div class="dashboard-box-wrapper">
+                        <div class="box-icons box-peso"><i class="fas fa-weight"></i></div>
+                        <div class="valor">XXX</div>
+                        <div class="tipo">XX</div>
+                    </div>
+                </div>
+                <div class="dashboard-box">
+                    <div class="dashboard-box-wrapper">
+                        <div class="box-icons box-pressao"><i class="fas fa-heartbeat"></i></i></div>
+                        <div class="valor">XXX</div>
+                        <div class="tipo">XXX</div>
+                    </div>
+                </div>
+                <div class="dashboard-box">
+                    <div class="dashboard-box-wrapper">
+                        <div class="box-icons box-imc"><i class="fas fa-calculator"></i></div>
+                        <div class="valor">XXX</div>
+                        <div class="tipo">XXX</div>
+                    </div>
+                </div>
+                <div class="dashboard-box">
+                    <div class="dashboard-box-wrapper">
+                        <div class="box-icons box-atividade"><i class="fas fa-biking"></i></div>
+                        <div class="valor">XXX</div>
+                        <div class="tipo">XXX</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</main>
 
 @endsection
---
+++ resources/views/app/layouts/_partials/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <i class="fas fa-bars" id="icon-menu" onclick="barraLateralResponsive()"></i>
+    <a href="{{ route('app.logout') }}"> <i class="fas fa-sign-out-alt"></i>
+        Sair
+    </a>
+</header>
