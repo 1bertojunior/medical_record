8467541d85c0a14561b21329e833d0209d3805d9
1bertojunior <hjunior854@gmail.com>
2023-11-15 11:51:22 -0300

    feat: Display list of users in the view
    Implemented the functionality to list and display users in the 'Usuários' page view. This commit enhances the user management experience in the admin section by showcasing a comprehensive list of users.

--- resources/views/app/users.blade.php
+++ resources/views/app/users.blade.php
@@ -29,13 +29,14 @@
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                      </tr>
-
+                        @foreach($users as $user)
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>Admin</td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
                 </div>
