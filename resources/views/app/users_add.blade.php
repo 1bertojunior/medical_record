d3c41d92987d0fb4311b750660407ee064ddf40f
1bertojunior <hjunior854@gmail.com>
2023-11-17 09:54:58 -0300

    feat: Display error messages in Users_Add view
    Implemented the display of error messages in the Users_Add view to provide users with feedback on form validation errors. This commit enhances the user experience by communicating issues during the user registration process.

--- resources/views/app/users_add.blade.php
+++ resources/views/app/users_add.blade.php
@@ -19,8 +19,14 @@
             <form method="post" action="{{ route('app.users.add') }}">
                 @csrf
                 <div class="form-group">
+                    @error('error')
+                        <span> {{$message }}</span>
+                    @enderror
                     <label for="name">Nome completo</label>
                     <input type="text" name="name" class="form-control" id="nome" placeholder="Seu nome completo">
+                    @error('name')
+                        <span> {{$message }}</span>
+                    @enderror
                 </div>
 
                 {{-- <div class="form-group">
@@ -35,11 +41,17 @@
                 <div class="form-group">
                     <label for="email">Endereço de email</label>
                     <input type="email" name="email" class="form-control" id="email" placeholder="Seu email">
+                    @error('email')
+                        <span> {{$message }}</span>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="password">Senha</label>
                     <input type="password" name="password" class="form-control" id="senha" placeholder="Senha">
+                    @error('passowrd')
+                        <span> {{$message }}</span>
+                    @enderror
                 </div>
                 <button type="submit" class="btn">Cadastrar</button>
             </form>
