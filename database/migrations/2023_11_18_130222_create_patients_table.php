6774b9d448bb519c74b014b3be291e6ae904df5d
1bertojunior <hjunior854@gmail.com>
2023-11-18 10:04:43 -0300

    feat: Create migration for 'patients'
    Added a migration file to create the 'patients' table in the database. This commit sets the groundwork for storing patient-related information in the admin section.

---
+++ database/migrations/2023_11_18_130222_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birth_date');
+            $table->string('cpf')->unique();
+            $table->string('phone');
+            $table->string('sus_card')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
