c089b7bd88e828a56a637c68e301a6309cda246f
1bertojunior <hjunior854@gmail.com>
2023-11-26 16:57:50 -0300

    feat(database): Add 'PatientsTableSeeder' to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(HealthcareProfessionTypesTableSeeder::class);
         $this->call(HealthcareProfessionalsTableSeeder::class);
+        $this->call(PatientsTableSeeder::class);
     }
 }
