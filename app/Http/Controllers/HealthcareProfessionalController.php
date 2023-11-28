7feb906d6e3cf32ecd725493c7527a76bc5d8abf
1bertojunior <hjunior854@gmail.com>
2023-11-28 09:18:22 -0300

    feat(controller): Implement create, store, edit, and update methods in HealthcareProfessionalController

---
+++ app/Http/Controllers/HealthcareProfessionalController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HealthcareProfessional;
+use App\Models\HealthcareProfessionType;
+use Illuminate\Http\Request;
+
+class HealthcareProfessionalController extends Controller
+{
+    public function create()
+    {
+        $title = 'Adicionar Profissional de Saúde';
+        $professionTypes = HealthcareProfessionType::all();
+
+        return view('app.professional_add', compact('title', 'professionTypes'));
+    }
+
+    public function store(Request $request)
+    {
+        $oldValues = $request->old();
+
+        // Validar o request
+        $request->validate(
+            [
+                'name' => 'required|min:5',
+                'document_number' => 'required|min:3',
+                'profession_type_id' => 'required',
+            ],
+            [
+                'name.required' => 'O campo de nome é obrigatório.',
+                'name.min' => 'O campo nome deve ter no mínimo :min caracteres.',
+                'document_number.required' => 'O campo de Documento é obrigatório.',
+                'document_number.min' => 'O Documento deve ter :min caracteres.',
+                'profession_type_id.required' => 'O campo Tipo de Profissional é obrigatório.',
+            ]
+        );
+
+        $professional = new HealthcareProfessional();
+        $professional->name = $request->name;
+        $professional->document_number = $request->document_number;
+        $professional->profession_type_id = $request->profession_type_id;
+
+        // Salvar o profissional de saúde no banco de dados
+        $result = $professional->save();
+
+        if ($result) {
+            return redirect()->route('app.healthcare_professionals')->with('success', 'Profissional de Saúde criado com sucesso!');
+        } else {
+            return redirect()->route('app.healthcare_professionals')->withErrors(['error' => 'Erro ao criar Profissional de Saúde, tente novamente!']);
+        }
+    }
+
+    public function edit($id)
+    {
+        $professional = HealthcareProfessional::findOrFail($id);
+        $professionTypes = HealthcareProfessionType::all();
+
+        return view('app.professional_edit', [
+            'title' => 'Editando Profissional de Saúde',
+            'professional' => $professional,
+            'professionTypes' => $professionTypes
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $professional = HealthcareProfessional::findOrFail($request->id);
+
+        $result = $professional->update($request->all());
+
+        if ($result) {
+            return redirect()->route('app.healthcare_professionals')->with('success', 'Profissional de Saúde editado com sucesso!');
+        } else {
+            return redirect()->route('app.healthcare_professionals')->withErrors(['error' => 'Erro ao editar Profissional de Saúde, tente novamente mais tarde!']);
+        }
+    }
+
+}
