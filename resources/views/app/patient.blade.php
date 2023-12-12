9207a99dfa44d0ea8309e26b0342f0d877970f28
1bertojunior <hjunior854@gmail.com>
2023-12-12 11:38:47 -0300

    fix: Correct display of information in Patients view

--- resources/views/app/patient.blade.php
+++ resources/views/app/patient.blade.php
@@ -61,12 +61,13 @@
                             <tr>
                                 <th scope="row">{{ $patient->id }}</th>
                                 <td>{{ $patient->name }}</td>
-                                <td>{{ $patient->cpf }}</td>
-                                <td>{{ $patient->sus_card }}</td>
+                                <td>{{ substr($patient->cpf, 0, 3) . '.' . substr($patient->cpf, 3, 3) . '.' . substr($patient->cpf, 6, 3) . '-' . substr($patient->cpf, 9, 2) }}</td>
+                                <td>{{ substr($patient->sus_card, 0, 3) . ' ' . '****' . ' ' . substr($patient->sus_card, 7, 1) . ' ' . substr($patient->sus_card, 8, 3) . ' ' . substr($patient->sus_card, 11) }}</td>
                                 <td>{{ $patient->phone }}</td>
-                                <td>{{ $patient->birth_date }}</td>
-                                <td>{{ $patient->notes }}</td>
+                                <td>{{ date('d/m/Y', strtotime($patient->birth_date)) }}</td>
+                                <td>{{ substr($patient->notes, 0, 50) }}</td>
                                 <td>
+                                    <a href="#modal" class="btn btn-info"><i class="fas fa-eye"></i></a>
                                     <a href="{{ route('app.patient.edit', ['id' => $patient->id]) }}" class="btn btn-warning"><i class="fas fa-edit"></i></a>
                                     <a href="#" class="btn btn-danger"><i class="fas fa-trash-alt"></i></a>
                                 </td>
