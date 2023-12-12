385bd3a820ae3e088f6d5b71761f3288e40c5b3a
1bertojunior <hjunior854@gmail.com>
2023-12-12 10:47:09 -0300

    fix: Correct HealthcareProfessionalsTableSeeder

--- database/seeders/HealthcareProfessionalsTableSeeder.php
+++ database/seeders/HealthcareProfessionalsTableSeeder.php
@@ -10,8 +10,13 @@ class HealthcareProfessionalsTableSeeder extends Seeder
     public function run()
     {
         $professionals = [
-            ['name' => 'Dr. Fulano', 'document_number' => '123456', 'profession_type_id' => 1],
-            ['name' => 'Enf. Beltrano', 'document_number' => '654321', 'profession_type_id' => 2],
+            ['name' => 'Fulano', 'document_number' => '123456', 'profession_type_id' => 1],
+            ['name' => 'Beltrano', 'document_number' => '654321', 'profession_type_id' => 2],
+            ['name' => 'Sicrano', 'document_number' => '789012', 'profession_type_id' => 3],
+            ['name' => 'Fulana', 'document_number' => '345678', 'profession_type_id' => 4],
+            ['name' => 'Beltrana', 'document_number' => '901234', 'profession_type_id' => 5],
+            ['name' => 'Sicrana', 'document_number' => '567890', 'profession_type_id' => 6],
+            ['name' => 'Fulano', 'document_number' => '234567', 'profession_type_id' => 7],
         ];
 
         foreach ($professionals as $professional) {
