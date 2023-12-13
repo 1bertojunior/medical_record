02f16309b68f8081461877a5f7564461dcb3b1f5
1bertojunior <hjunior854@gmail.com>
2023-12-13 15:13:52 -0300

    fix: Correct form in Medical Records ADD view

--- resources/views/app/medical_records_add.blade.php
+++ resources/views/app/medical_records_add.blade.php
@@ -16,7 +16,7 @@
         </div>
 
         <div class="dashboard-conteudo">
-            <form method="post" action="{{ route('app.medical_records.add') }}">
+            {{-- <form method="post" action="{{ route('app.medical_records.add') }}">
                 @csrf
                 
                 <div class="form-group">
@@ -65,6 +65,87 @@
                     @enderror
                 </div>
 
+                <button type="submit" class="btn btn-secondary">Cadastrar</button>
+            </form> --}}
+            <form method="post" action="{{ route('app.medical_records.add') }}" enctype="multipart/form-data">
+                @csrf
+            
+                <div class="form-group">
+                    <div class="form-group">
+                        <!-- Selecione o paciente -->
+                        <label for="patient_id">Paciente*</label>
+                        <select name="patient_id" class="form-control">
+                            @foreach($patients as $patient)
+                                <option value="{{ $patient->id }}" {{ old('patient_id') == $patient->id ? 'selected' : '' }}>
+                                    {{ $patient->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('patient_id')
+                            <span> {{$message }}</span>
+                        @enderror
+                    </div>
+                    <label for="healthcare_professional_id">Profissional*</label>
+                    <select name="healthcare_professional_id" class="form-control">
+                        @foreach($professionals as $professional)
+                            <option value="{{ $professional->id }}" {{ old('healthcare_professional_id') == $professional->id ? 'selected' : '' }}>
+                                {{ $professional->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('healthcare_professional_id')
+                        <span> {{$message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="file_path">Arquivo</label>
+                    <input type="file" name="file_path" class="form-control">
+                    @error('file_path')
+                        <span> {{$message }}</span>
+                    @enderror
+                </div>
+            
+                <div class="form-group">
+                    <label for="complaints">Queixa doença</label>
+                    <textarea name="complaints" class="form-control">{{ old('complaints') }}</textarea>
+                    @error('complaints')
+                        <span> {{$message }}</span>
+                    @enderror
+                </div>
+            
+                <div class="form-group">
+                    <label for="disease_history">Histórico de doenças</label>
+                    <textarea name="disease_history" class="form-control">{{ old('disease_history') }}</textarea>
+                    @error('disease_history')
+                        <span> {{$message }}</span>
+                    @enderror
+                </div>
+            
+                <div class="form-group">
+                    <label for="allergies">Alergias</label>
+                    <textarea name="allergies" class="form-control">{{ old('allergies') }}</textarea>
+                    @error('allergies')
+                        <span> {{$message }}</span>
+                    @enderror
+                </div>
+            
+                <div class="form-group">
+                    <label for="diagnosis">Diagnóstico</label>
+                    <textarea name="diagnosis" class="form-control">{{ old('diagnosis') }}</textarea>
+                    @error('diagnosis')
+                        <span> {{$message }}</span>
+                    @enderror
+                </div>
+            
+                <div class="form-group">
+                    <label for="follow_up_instructions">Instruções de acompanhamento</label>
+                    <textarea name="follow_up_instructions" class="form-control">{{ old('follow_up_instructions') }}</textarea>
+                    @error('follow_up_instructions')
+                        <span> {{$message }}</span>
+                    @enderror
+                </div>
+            
                 <button type="submit" class="btn btn-secondary">Cadastrar</button>
             </form>
             <div class="dashboard-flex">
