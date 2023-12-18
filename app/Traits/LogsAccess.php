eb3ba7a6bd2f3feee088f45a6867c0978eb43e3c
1bertojunior <hjunior854@gmail.com>
2023-12-18 13:45:57 -0300

    feat: Create LogsAccess trait
    Implemented the LogsAccess trait to encapsulate methods related to logging access data in the admin section. This commit enhances modularity and organization of code.

---
+++ app/Traits/LogsAccess.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\AccessLog;
+use Exception;
+
+trait LogsAccess
+{
+    protected static function booted()
+    {
+        static::created(function ($model) {
+            static::createAccessLog('create', $model);
+        });
+
+        static::updated(function ($model) {
+            static::createAccessLog('update', $model);
+        });
+
+        static::deleted(function ($model) {
+            static::createAccessLog('delete', $model);
+        });
+    }
+
+    protected static function createAccessLog($action, $model)
+    {
+        AccessLog::create([
+            'action' => $action,
+            'model' => get_class($model),
+            'model_id' => $model->id,
+            'ip_address' => request()->ip(),
+            'user_id' => auth()->id(),
+            'details' => json_encode(request()->all()),
+            'result' => 'success',
+        ]);
+    }
+}
