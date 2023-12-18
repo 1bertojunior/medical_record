e4ee045a303b7d83abd30ee14a4e306282fa171f
1bertojunior <hjunior854@gmail.com>
2023-12-18 13:50:57 -0300

    feat: Implement LogsAccess trait in User model
    Added 'use HasFactory, LogsAccess;' in the User model, utilizing the LogsAccess trait to handle logging access data related to user actions in the admin section.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Traits\LogsAccess;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, LogsAccess;
 
     /**
      * The attributes that are mass assignable.
