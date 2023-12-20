bf2b7ae181ca8870a21286bb7ac4ff8ba76209d4
1bertojunior <hjunior854@gmail.com>
2023-12-20 01:12:54 -0300

    feat: Add details routes for all models in web.php
    Added details routes for each model in web.php, pointing to their respective 'details' methods in the AppController. This commit establishes routes for viewing detailed information for various entities in the admin section.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccessLogController;
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HealthcareProfessionalController;
@@ -26,36 +27,39 @@ Route::middleware('authentication')->prefix('/app')->group(function() {
     Route::post('users/add', [UserController::class, 'create'])->name('app.users.add');
     Route::get('users/edit/{id}', [UserController::class, 'edit'])->name('app.users.edit');
     Route::put('users/edit/{id}', [UserController::class, 'update'])->name('app.users.edit');
+    Route::get('users/details/{id}', [UserController::class, 'details'])->name('app.users.details');
 
     Route::get('/patient', [AppController::class, 'patient'])->name('app.patient');
     Route::get('/patient/add', [PatientController::class, 'create'])->name('app.patient.add');
     Route::post('/patient/add', [PatientController::class, 'store'])->name('app.patient.add');
     Route::get('/patient/edit/{id}', [PatientController::class, 'edit'])->name('app.patient.edit');
     Route::put('/patient/edit/{id}', [PatientController::class, 'update'])->name('app.patient.edit');
+    Route::get('/patient/details/{id}', [PatientController::class, 'details'])->name('app.patient.details');
 
     Route::get('/healthcare_profession_type', [AppController::class, 'healthcareProfessionType'])->name('app.healthcare_profession_type');
     Route::get('/healthcare_profession_type/add', [HealthcareProfessionTypeController::class, 'create'])->name('app.healthcare_profession_type.add');
     Route::post('/healthcare_profession_type/add', [HealthcareProfessionTypeController::class, 'store'])->name('app.healthcare_profession_type.add');
     Route::get('/healthcare_profession_type/edit/{id}', [HealthcareProfessionTypeController::class, 'edit'])->name('app.healthcare_profession_type.edit');
     Route::put('/healthcare_profession_type/edit/{id}', [HealthcareProfessionTypeController::class, 'update'])->name('app.healthcare_profession_type.edit');
+    Route::get('/healthcare_profession_type/details/{id}', [HealthcareProfessionTypeController::class, 'details'])->name('app.healthcare_profession_type.details');
 
-    // Rotas para Profissionais de Saúde
     Route::get('/healthcare_professionals', [AppController::class, 'healthcareProfessionals'])->name('app.healthcare_professionals');
     Route::get('/healthcare_professionals/add', [HealthcareProfessionalController::class, 'create'])->name('app.healthcare_professionals.add');
     Route::post('/healthcare_professionals/add', [HealthcareProfessionalController::class, 'store'])->name('app.healthcare_professionals.add');
     Route::get('/healthcare_professionals/edit/{id}', [HealthcareProfessionalController::class, 'edit'])->name('app.healthcare_professionals.edit');
     Route::put('/healthcare_professionals/edit/{id}', [HealthcareProfessionalController::class, 'update'])->name('app.healthcare_professionals.edit');
     Route::delete('/healthcare_professionals/delete/{id}', [HealthcareProfessionalController::class, 'destroy'])->name('app.healthcare_professionals.delete');
-
-    // Rotas de Prontuários
+    Route::get('/healthcare_professionals/details/{id}', [HealthcareProfessionalController::class, 'details'])->name('app.healthcare_professionals.details');
+    
     Route::get('/medical-records', [AppController::class, 'medicalRecords'])->name('app.medical_records');
     Route::get('/medical-records/add', [MedicalRecordController::class, 'create'])->name('app.medical_records.add');
     Route::post('/medical-records/add', [MedicalRecordController::class, 'store'])->name('app.medical_records.add');
     Route::get('/medical-records/edit/{id}', [MedicalRecordController::class, 'edit'])->name('app.medical_records.edit');
     Route::put('/medical-records/edit/{id}', [MedicalRecordController::class, 'update'])->name('app.medical_records.edit');
     Route::delete('/medical-records/delete/{id}', [MedicalRecordController::class, 'destroy'])->name('app.medical_records.delete');
+    Route::get('/medical-records/details/{id}', [MedicalRecordController::class, 'details'])->name('app.medical_records.details');
 
-    // Access Logs
     Route::get('/logs-access', [AppController::class, 'logsAccess'])->name('app.logs_access');
+    Route::get('/logs-access/details/{id}', [AccessLogController::class, 'details'])->name('app.logs_access.details');
 
 });
