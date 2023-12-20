6c130a4808476435c5395215a3080b2cfa733b8b
1bertojunior <hjunior854@gmail.com>
2023-12-20 01:13:39 -0300

    feat: Create 'model.blade.php' for details view
    Implemented the 'model.blade.php' file to serve as a template for displaying detailed information about a specific model entity in the admin section. This commit establishes a consistent structure for the details view.

---
+++ resources/views/app/layouts/_partials/modal.blade.php
@@ -0,0 +1,19 @@
+<!-- Modal -->
+<div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="viewModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="viewModalLabel">Detalhes do </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        </div>
+      </div>
+    </div>
+  </div>
