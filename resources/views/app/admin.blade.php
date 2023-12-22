38fdf05e851975c18e9e6ea48d9f04613a69873d
1bertojunior <hjunior854@gmail.com>
2023-12-22 18:16:52 -0300

    style: Enhance admin panel introduction
    Improved code structure and style for better readability. Utilized dynamic CSS classes, separated blocks, and organized code for easier maintenance.

--- resources/views/app/admin.blade.php
+++ resources/views/app/admin.blade.php
@@ -9,50 +9,43 @@
 <main id="main-conteudo-total">
 
     @include('app.layouts._partials.header')
-    
 
     <div class="main-conteudo">
-        <div class="conteudo-titulo-main">
-            <h2> Dashboard</h2>
+        
+        <!-- Bloco de Estatísticas -->
+        <div class="dashboard-conteudo">
+            <div class="conteudo-titulo-main">
+                <h2>Dashboard</h2>
+            </div>
+            
+            <div class="dashboard-flex">
+                @foreach ($data['statistics'] as $statistic)
+                    <div class="dashboard-box">
+                        <div class="dashboard-box-wrapper">
+                            <div class="box-icons {{ $statistic['iconClass'] }}"><i class="fas {{ $statistic['icon'] }}"></i></div>
+                            <div class="valor">{{ $statistic['value'] }}</div>
+                            <div class="tipo">{{ $statistic['label'] }}</div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
 
+        <!-- Área de Introdução -->
         <div class="dashboard-conteudo">
+            <div class="conteudo-titulo-main">
+                <h2>Bem-vindo ao Painel de Controle</h2>
+            </div>
+            
             <div class="dashboard-flex">
-                <div class="dashboard-box">
-                    <div class="dashboard-box-wrapper">
-                        <div class="box-icons box-peso"><i class="fas fa-user"></i></div>
-                        <div class="valor">{{ $data['totalUsers'] }}</div>
-                        <div class="tipo">Usuários</div>
-                    </div>
-                </div>
-                <div class="dashboard-box">
-                    <div class="dashboard-box-wrapper">
-                        <div class="box-icons box-pressao"><i class="fas fa-procedures"></i></div>
-                        <div class="valor">{{ $data['totalPatients'] }}</div>
-                        <div class="tipo">Pacientes</div>
-                    </div>
-                </div>
-                <div class="dashboard-box">
-                    <div class="dashboard-box-wrapper">
-                        <div class="box-icons box-imc"><i class="fas fa-stethoscope"></i></div>
-                        <div class="valor">{{ $data['totalProfessionTypes'] }}</div>
-                        <div class="tipo">Tipos de Profissionais</div>
-                    </div>
-                </div>
-                <div class="dashboard-box">
-                    <div class="dashboard-box-wrapper">
-                        <div class="box-icons box-atividade"><i class="fas fa-user-md"></i></div>
-                        <div class="valor">{{ $data['totalProfessionals'] }}</div>
-                        <div class="tipo">Profissionais</div>
+                <div class="introduction card">
+                    <div class="card-body">
+                        <p class="card-text text-justify text-muted mb-0">A ONG Amigos da Comunidade é um projeto sem fins lucrativos onde todos que estão envolvidos são voluntários e visam trazer o melhor para a comunidade. Desde 2016 trabalhamos em Picos e várias cidades do interior do Piauí, onde são realizados em média 2000 atendimentos em saúde nos dias e locais do evento. Essa iniciativa se faz como grande potencializador das capacidades técnico-científicas dos alunos pertencentes à área da saúde e outras áreas. Pelo fato de propiciar aos voluntários práticas de suma importância para a sua formação. E os profissionais em questão tenham uma maior formação no sentido de contato humanista de sua profissão e experiência de trabalho em equipe.</p>
                     </div>
                 </div>
             </div>
-            
-
         </div>
-
-        
-
+    
     </div>
 
 </main>
