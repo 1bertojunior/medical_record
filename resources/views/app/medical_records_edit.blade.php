2564acc43d26af435d47d8a6d1f8e94cce3db6a8
1bertojunior <hjunior854@gmail.com>
2023-12-22 18:00:59 -0300

    fix: Corrected attributes in medical_records_edit view
    Addressed an issue in the medical_records_edit view by ensuring the correct attributes are used for rendering, providing accurate and complete information in the user interface.

--- resources/views/app/medical_records_edit.blade.php
+++ resources/views/app/medical_records_edit.blade.php
@@ -18,64 +18,96 @@
             <div class="dashboard-conteudo">
                 <form method="post" action="{{ route('app.medical_records.edit', ['id' => $medicalRecord->id]) }}" enctype="multipart/form-data">
                     @csrf
-                    @method('PUT')
+                    @method('PUT')                                            
+
+                    <div class="form-group">
+                        <label for="patient_info">Informações do Paciente</label>
+                        <textarea class="form-control" id="patient_info" readonly>{{ $medicalRecord->patient->name }}</textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="professional_info">Informações do Profissional de Saúde</label>
+                        <textarea class="form-control" id="professional_info" readonly>{{ $medicalRecord->healthcareProfessional->name }}</textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="file">Arquivo atual</label>
+                        @if($medicalRecord->file_path)
+                            @if(in_array(pathinfo($medicalRecord->file_path, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png']))
+                                <div><img class="img-preview" src="{{ asset($medicalRecord->file_path) }}" alt="Preview do Arquivo" class="img-preview"></div>
+                            @elseif(in_array(pathinfo($medicalRecord->file_path, PATHINFO_EXTENSION), ['pdf']))
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="{{ asset($medicalRecord->file_path) }}" allowfullscreen></iframe>
+                                </div>
+                            @endif
+                            
+                        @else
+                            <p>Nenhum arquivo anexado</p>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label for="file_path" class="form-label">Arquivo (JPEG, PNG, PDF - Máximo 2MB)</label>
+                        <input type="file" name="file_path" class="form-control" id="file_path">
+                        @error('file_path')
+                            <span> {{$message }}</span>
+                        @enderror
+                    </div>
 
                     <div class="form-group">
                         @error('error')
                             <span> {{ $message }}</span>
                         @enderror
-                        <label for="chief_complaint">Queixa Principal</label>
-                        <textarea name="chief_complaint" class="form-control" id="chief_complaint">{{ $medicalRecord->chief_complaint }}</textarea>
-                        @error('chief_complaint')
+                        <label for="complaints">Queixa Principal</label>
+                        <textarea name="complaints" class="form-control" id="complaints">{{ $medicalRecord->complaints }}</textarea>
+                        @error('complaints')
                             <span> {{ $message }}</span>
                         @enderror
                     </div>
 
                     <div class="form-group">
-                        <label for="history_of_present_illness">História da Doença Atual</label>
-                        <textarea name="history_of_present_illness" class="form-control" id="history_of_present_illness">{{ $medicalRecord->history_of_present_illness }}</textarea>
-                        @error('history_of_present_illness')
+                        @error('error')
+                            <span> {{ $message }}</span>
+                        @enderror
+                        <label for="disease_history">Histórico de doenças</label>
+                        <textarea name="disease_history" class="form-control" id="disease_history">{{ $medicalRecord->disease_history }}</textarea>
+                        @error('disease_history')
                             <span> {{ $message }}</span>
                         @enderror
                     </div>
 
                     <div class="form-group">
-                        <label for="past_medical_history">História Médica Pregressa</label>
-                        <textarea name="past_medical_history" class="form-control" id="past_medical_history">{{ $medicalRecord->past_medical_history }}</textarea>
-                        @error('past_medical_history')
+                        @error('error')
+                            <span> {{ $message }}</span>
+                        @enderror
+                        <label for="allergies">Alergias</label>
+                        <textarea name="allergies" class="form-control" id="allergies">{{ $medicalRecord->allergies }}</textarea>
+                        @error('allergies')
                             <span> {{ $message }}</span>
                         @enderror
                     </div>
 
                     <div class="form-group">
-                        <label for="image_path">URL da Imagem</label>
-                        <input type="text" name="image_path" class="form-control" id="image_path" placeholder="URL da Imagem" value="{{ $medicalRecord->image_path }}">    
-                        @error('image_path')
+                        @error('error')
+                            <span> {{ $message }}</span>
+                        @enderror
+                        <label for="diagnosis">Diagnóstico</label>
+                        <textarea name="diagnosis" class="form-control" id="diagnosis">{{ $medicalRecord->diagnosis }}</textarea>
+                        @error('diagnosis')
                             <span> {{ $message }}</span>
                         @enderror
                     </div>
 
                     <div class="form-group">
-                        <label for="image">Imagem</label>
-                        <input type="file" name="image" class="form-control" id="image">
-                        <!-- Adicione campo para upload de imagem -->
-                        @error('image')
+                        @error('error')
                             <span> {{ $message }}</span>
                         @enderror
-                    
-                        @if ($medicalRecord->image_path)
-                            <div class="mt-2">
-                                <label>Imagem Atual:</label>
-                                <div class="d-flex align-items-center">
-                                    <span>{{ basename($medicalRecord->image_path) }}</span>
-                                    <span class="ml-2"> | </span>
-                                    <label for="remove_image">Remover:</label>
-                                    <input type="checkbox" name="remove_image" id="remove_image" value="1">
-                                </div>
-                            </div>
-                        @endif
+                        <label for="follow_up_instructions">Instruções de acompanhamento</label>
+                        <textarea name="follow_up_instructions" class="form-control" id="follow_up_instructions">{{ $medicalRecord->follow_up_instructions }}</textarea>
+                        @error('follow_up_instructions')
+                            <span> {{ $message }}</span>
+                        @enderror
                     </div>
-                    
 
                     <button type="submit" class="btn btn-secondary">Atualizar</button>
                 </form>
