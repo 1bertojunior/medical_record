41a542a95041ab88959783cb4c34198e21a5b692
1bertojunior <hjunior854@gmail.com>
2023-12-21 23:49:35 -0300

    feat: Implement logo in responsive header
    Added implementation for displaying the logo in the responsive header of the admin section. This commit ensures a visually appealing and responsive design for the logo, enhancing the overall user experience.

--- resources/views/app/layouts/_partials/header.blade.php
+++ resources/views/app/layouts/_partials/header.blade.php
@@ -1,5 +1,6 @@
 <header>
     <i class="fas fa-bars" id="icon-menu" onclick="barraLateralResponsive()"></i>
+    <img class="logo" src="{{ asset('img/amigos-símbolo_dark.png') }}" alt="">
     <a href="{{ route('app.logout') }}"> <i class="fas fa-sign-out-alt"></i>
         Sair
     </a>
